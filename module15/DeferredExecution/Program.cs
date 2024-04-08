5cbddcfd09de4cd0bf7c17bd8d593a19a47d1be6
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-08 20:49:53 +0200

    Fin módulo

--- module15/DeferredExecution/Program.cs
+++ module15/DeferredExecution/Program.cs
@@ -4,7 +4,14 @@
 SamplesViewModel vm = new();
 
 // Call Sample Method
-var result = vm.DeferredExecution();
+//var result = vm.DeferredExecution();
+//var result = vm.DeferredExecutionEnumerator();
+//var result = vm.UsingWhereAndTake();
+//var result = vm.UsingSimpleFilter();
+//var result = vm.UsingSimpleFilterAndTake();
+//var result = vm.UsingYield();
+//var result = vm.UsingYieldAndTake();
+var result = vm.UsingYieldAndOrderBy();
 
 // Display Results
 vm.Display(result);
