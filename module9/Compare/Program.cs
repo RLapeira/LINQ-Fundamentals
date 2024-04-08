c78ade59657cb373a3f5ef494dbb432cb1313beb
Rocío Lapeira Naranjo <r.lapeira.naranjo@avanade.com>
2024-04-08 08:40:06 +0200

    Using SequenceEqual() with Integer Collection

---
+++ module9/Compare/Program.cs
@@ -0,0 +1,11 @@
+﻿using LINQSamples;
+
+// Create instance of view model
+SamplesViewModel vm = new();
+
+// Call Sample Method
+//var result = vm.SequenceEqualIntegersQuery();
+var result = vm.SequenceEqualIntegersMethod();
+
+// Display Results
+vm.Display(result);
